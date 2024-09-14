f05846687432db26cf370dc47d4800f5b070ffc6
nisaolgun <144125330+nisaolgun@users.noreply.github.com>
2024-09-14 21:24:32 +0300

    Edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('backgroundChange', [
+      state('day', style({
+        backgroundColor: '#007BA7',
+        backgroundImage: 'url()'
+      })),
+      state('night', style({
+        backgroundColor: '#003153',
+        backgroundImage: 'url()'
+      })),
+      transition('day <=> night', animate('1s ease-in-out'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'ViewTransitionAPI';
+  currentState = 'day';
+
+  toggleTheme() {
+    this.currentState = this.currentState === 'day' ? 'night' : 'day';
+  }
+
+  get buttonIcon() {
+    return this.currentState === 'day' ? '☀️' : '🌙';
+  }
+
+  get buttonText() {
+    return this.currentState === 'day' ? 'Gece Moduna Geç' : 'Gündüz Moduna Geç';
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import edin
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    BrowserAnimationsModule, 
+    ...appConfig.providers 
+  ]
+})
+.catch((err) => console.error(err));
